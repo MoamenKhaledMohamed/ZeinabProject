b81cef04a4b09b9cfcf36467d5c0c36345b18fe1
MoamenKhaledMohamed <moamen2020khaled@gmail.com>
2020-12-24 22:13:07 +0200

    add controllres and other files

--- main.go
+++ main.go
@@ -1,23 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-// listen to server
+	"github.com/gorilla/mux"
 
-// create routes for book
-// add a reader,
-// remove a reader,
-// search for a reader by id or name, and
-// get readers info
-
-// create routes for readers
-// add a book,
-// search for a book by id or name,
-// load books info,
-// save books info,
-// sort books by title, and
-// sort by publication date
+	C "./controllers"
+)
 
 func main() {
-	fmt.Print("hello")
+	// Declare a new router
+	r := mux.NewRouter()
+
+	// ############# Routes For Reader ###########
+	// add a reader
+	// r.HandleFunc("/reader", Handler).Methods("POST")
+
+	// // delete a reader
+	// r.HandleFunc("/reader/{id}", Handler).Methods("DELETE")
+
+	// // get readers
+	// r.HandleFunc("/readers", Handler).Methods("GET")
+
+	// // search by id
+	// r.HandleFunc("/reader/{id}", Handler).Methods("GET")
+
+	// // search by name
+	// r.HandleFunc("/reader/{name}", Handler).Methods("GET")
+
+	// ############ End Routes ###################
+
+	// ############ Routes For Bookstore #########
+
+	// add a book
+	// r.HandleFunc("/book", Handler).Methods("POST")
+
+	// search by id
+	// r.HandleFunc("/book/{id}", Handler).Methods("GET")
+
+	// // search by name
+	// r.HandleFunc("/book/{name}", Handler).Methods("GET")
+
+	// // get books
+	r.HandleFunc("/books", C.Handler).Methods("GET")
+
+	// // edit book
+	// r.HandleFunc("/book/{id}", Handler).Methods("PUT")
+
+	// // sort by title or publication
+	// r.HandleFunc("/book/sort-by-title", Handler).Methods("GET")
+	// r.HandleFunc("/book/sort-by-publication", Handler).Methods("GET")
+
+	// ############ End Routes ####################
+
+	// server listens on port 8080
+	http.ListenAndServe(":8080", r)
 }
