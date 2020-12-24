9d3b2c5931490eb824935ea6b11576dc6e9c6d19
MoamenKhaledMohamed <moamen2020khaled@gmail.com>
2020-12-24 15:56:59 +0200

    create main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// listen to server
+
+// create routes for book
+// add a reader,
+// remove a reader,
+// search for a reader by id or name, and
+// get readers info
+
+// create routes for readers
+// add a book,
+// search for a book by id or name,
+// load books info,
+// save books info,
+// sort books by title, and
+// sort by publication date
+
+func main() {
+	fmt.Print("hello")
+}
